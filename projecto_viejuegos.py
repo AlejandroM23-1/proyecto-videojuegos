9a128c03661d9fa33eaada4c30b0ce4f4ddbfa9b
AlejandroM23-1 <1588479@senati.pe>
2026-02-07 19:01:46 +0000

    Update projecto_viejuegos.py

--- projecto_viejuegos.py
+++ projecto_viejuegos.py
@@ -26,13 +26,19 @@ clientes={
     }
 juegos= ["Honkai: Star rail", "Left 4 dead 2", "Counter Strike", "Genshin Impact", "Wuthering Waves", "Silent Hill"]
 
-def sumario(juego):
+def resumen_juego(juego):
     print(f"\nResumen de | {juego}:\n")
     print(f"\t Género del juego: {generos[juego]}")
     print(f"\t Total de ventas para este juego: {ventas_y_stock[juego][0]} unidades.")
     print(f"\t Total de stock para este juego: {ventas_y_stock[juego][1]} unidades.")
     print(f"\t Clientes que han adquirido el juego: {', '.join(clientes[juego])}")
 
-for juego in juegos:
-    sumario(juego)
-    
+limite= 500
+def mostrar_resumenes():
+    for juego in juegos:
+        if ventas_y_stock[juego][0] > limite:
+            resumen_juego(juego)
+mostrar_resumenes()    
+ventas_totales = lambda: sum(ventas for juego, (ventas, _) in ventas_y_stock.items() if ventas_y_stock[juego][0] > limite)
+
+print(f"\n El total de ventas de todos los productos han sido {ventas_totales()} productos")
